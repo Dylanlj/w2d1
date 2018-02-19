832b08f9c1d09001380fde07ab1c0771d4f7d4cd
Dylanlj <dylanl-j@hotmail.ca>
2018-02-19 16:12:48 +0000

    finished step 4

---
+++ http-requests/step4.js
@@ -0,0 +1,25 @@
+function getHTML (options, callback) {
+  var https = require('https')
+  var buffer = '';
+  https.get(options, function(response){
+
+    response.on('data', function(data){
+      buffer += data
+    })
+
+    response.on('end', function(){
+      callback(buffer)
+    })
+  })
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML)
